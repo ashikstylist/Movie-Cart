4edbffc50aa85d20c16cf018b652a3653edf3725
ashikstylist <41476160+ashikstylist@users.noreply.github.com>
2021-08-02 01:28:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ let corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+
 app.get('/api', function(req, res) {
     var url = 'https://api.themoviedb.org/3/discover/movie?api_key=3d485e84c7ae1856fb134fefd31ed2df&language=en-US&sort_by=popularity.desc&include_video=false&page=1';
     fetch(url)
