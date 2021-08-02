5255b3af8eaac83b5e4ef36e6ffdbb01893fb66c
ashikstylist <41476160+ashikstylist@users.noreply.github.com>
2021-08-02 00:43:43 -0700

    Code Smell Resolved

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/api', function (req, res) {
+app.get('/api', () => {
     var url = 'https://api.themoviedb.org/3/discover/movie?api_key=3d485e84c7ae1856fb134fefd31ed2df&language=en-US&sort_by=popularity.desc&include_video=false&page=1';
     fetch(url)
     .then(res => res.json())
