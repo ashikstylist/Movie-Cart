69899701fd1d93315ec451da9600b6a736c879d9
ashikstylist <41476160+ashikstylist@users.noreply.github.com>
2021-08-02 01:19:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.get('/api', function(req, res) {
     var url = 'https://api.themoviedb.org/3/discover/movie?api_key=3d485e84c7ae1856fb134fefd31ed2df&language=en-US&sort_by=popularity.desc&include_video=false&page=1';
     fetch(url)
-    .then(res => res.json())
+    .then(result => result.json())
     .then(data => {
         res.send({ data });
     })
